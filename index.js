2b7d4e014976299ced995e8dab374fa8423cc01c
Gurkaran Singh <sgurkaran2000@gmail.com>
2019-12-27 12:47:46 +0530

    added try catch

--- index.js
+++ index.js
@@ -19,215 +19,257 @@ app.use(bodyParser.json());
 
 app.get('/:phone', (REQ, RES) => {
     var OTP = Math.floor(10000 + Math.random() * 90000).toString()
-    TempUser.create({ phone: REQ.params.phone.split(" ")[1].trim(), otp: OTP })
-        .then(u => {
-            if (u) {
-                var unirest = require("unirest");
-                var req = unirest("POST", "https://www.fast2sms.com/dev/bulk");
-                req.headers({
-                    "content-type": "application/x-www-form-urlencoded",
-                    "cache-control": "no-cache",
-                    "authorization": "oUvs89JdohAlMAkzdLn7vLiUTzBi0fiHVoD4pJPVQ4X1vM8kw2O6YwYydalr"
-                });
-                req.form({
-                    "sender_id": "FSTSMS",
-                    "language": "english",
-                    "route": "qt",
-                    "numbers": REQ.params.phone.split(" ")[1].trim(),
-                    "message": "17485",
-                    "variables": "{#AA#}",
-                    "variables_values": OTP.toString()
-                });
-                req.end(function (res) {
-                    if (res.error) throw new Error(res.error);
-                    console.log(res.body);
-                });
-                RES.send({
-                    otp: OTP
-                })
-            }
-        })
+    try {
+        TempUser.create({ phone: REQ.params.phone.split(" ")[1].trim(), otp: OTP })
+            .then(u => {
+                if (u) {
+                    var unirest = require("unirest");
+                    var req = unirest("POST", "https://www.fast2sms.com/dev/bulk");
+                    req.headers({
+                        "content-type": "application/x-www-form-urlencoded",
+                        "cache-control": "no-cache",
+                        "authorization": "oUvs89JdohAlMAkzdLn7vLiUTzBi0fiHVoD4pJPVQ4X1vM8kw2O6YwYydalr"
+                    });
+                    req.form({
+                        "sender_id": "FSTSMS",
+                        "language": "english",
+                        "route": "qt",
+                        "numbers": REQ.params.phone.split(" ")[1].trim(),
+                        "message": "17485",
+                        "variables": "{#AA#}",
+                        "variables_values": OTP.toString()
+                    });
+                    req.end(function (res) {
+                        if (res.error) throw new Error(res.error);
+                        console.log(res.body);
+                    });
+                    RES.send({
+                        otp: OTP
+                    })
+                }
+            })
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 app.get('/register-user/:phone/:token', (REQ, RES) => {
-    User.create({ phone: REQ.params.phone, token: REQ.params.token })
-        .then(u => {
-            console.log(u.user)
-            RES.send(u)
-        })
+    try {
+        User.create({ phone: REQ.params.phone, token: REQ.params.token })
+            .then(u => {
+                console.log(u.user)
+                RES.send(u)
+            })
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 app.get('/find-user/:phone', (REQ, RES) => {
-    User.findAll({
-        where: {
-            phone: REQ.params.phone
-        }
-    })
-        .then(u => {
-            if (u) {
-                RES.send({
-                    message: u
-                })
-            } else {
-                RES.send({
-                    message: "404"
-                })
+    try {
+        User.findAll({
+            where: {
+                phone: REQ.params.phone
             }
         })
+            .then(u => {
+                if (u) {
+                    RES.send({
+                        message: u
+                    })
+                } else {
+                    RES.send({
+                        message: "404"
+                    })
+                }
+            })
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 app.get('/send-request/:receiver_id/:sender_id/:senderphone', (REQ, RES) => {
-    User.findAll({
-        where: {
-            id: parseInt(REQ.params.receiver_id)
-        },
-        raw: true
-    })
-        .then(u => {
-            Fadmin.messaging().sendToDevice(u[0].token, {
-                notification: {
-                    title: "Notification",
-                    body: REQ.params.senderphone + " wants to text you!",
-                    sound: "default"
-                },
-                data: {
-                    "sendername": "IBIG",
-                    "message": "its a Request Message!"
-                }
-            })
-                .then(r => {
-                    RES.sendStatus(200)
+    try {
+        User.findAll({
+            where: {
+                id: parseInt(REQ.params.receiver_id)
+            },
+            raw: true
+        })
+            .then(u => {
+                Fadmin.messaging().sendToDevice(u[0].token, {
+                    notification: {
+                        title: "Notification",
+                        body: REQ.params.senderphone + " wants to text you!",
+                        sound: "default"
+                    },
+                    data: {
+                        "sendername": "IBIG",
+                        "message": "its a Request Message!"
+                    }
                 })
+                    .then(r => {
+                        RES.sendStatus(200)
+                    })
                 RequestModal.create({
                     to: parseInt(REQ.params.receiver_id),
                     from: parseInt(REQ.params.sender_id),
                     phone_of_from: REQ.params.senderphone
                 })
-        })
+            })
+    } catch (error) {
+        console.log(error)
+    }
 })
-app.post('/save-message-online',(REQ,RES) => {
-    var res = JSON.parse(REQ.body.data);
-    Messages.create({
-        message: res.message,
-        sender: parseInt(res.sender),
-        receiver: parseInt(res.receiver_id),
-        sender_phone: res.receiver_id,
-        read: false,
-        received: true
-    })
-    .then(u => {
-        RES.send(u)
-    })
-})
-app.post('/save-message', (REQ, RES) => {
-    console.log(REQ.body)
-    console.log(typeof(REQ.body))
-    var res = JSON.parse(REQ.body.data);
-    if(res.code !== "#<REQUEST>#" || res.code === null){
+app.post('/save-message-online', (REQ, RES) => {
+    try {
+        var res = JSON.parse(REQ.body.data);
         Messages.create({
             message: res.message,
             sender: parseInt(res.sender),
             receiver: parseInt(res.receiver_id),
-            read: false
+            sender_phone: res.receiver_id,
+            read: false,
+            received: true
         })
-        .then(u => {
-            RES.sendStatus(200)
-        })
-    }else{
-        RES.sendStatus(200)
+            .then(u => {
+                RES.send(u)
+            })
+    } catch (error) {
+        console.log(error)
     }
-    User.findAll({
-        where: {
-            id: res.receiver_id
+})
+app.post('/save-message', (REQ, RES) => {
+    try {
+        var res = JSON.parse(REQ.body.data);
+        if (res.code !== "#<REQUEST>#" || res.code === null) {
+            Messages.create({
+                message: res.message,
+                sender: parseInt(res.sender),
+                receiver: parseInt(res.receiver_id),
+                read: false
+            })
+                .then(u => {
+                    RES.sendStatus(200)
+                })
+        } else {
+            RES.sendStatus(200)
         }
-    })
-    .then(r => {
-        Fadmin.messaging().sendToDevice(r[0].token, {
-            notification: {
-                title: "Notification",
-                body: res.message, 
-                sound: "default"
-            },
-            data: {
-                "sendername": "IBIG",
-                "message": "Request Accepted"
+        User.findAll({
+            where: {
+                id: res.receiver_id
             }
         })
-    })
+            .then(r => {
+                Fadmin.messaging().sendToDevice(r[0].token, {
+                    notification: {
+                        title: "Notification",
+                        body: res.message,
+                        sound: "default"
+                    },
+                    data: {
+                        "sendername": "IBIG",
+                        "message": "Request Accepted"
+                    }
+                })
+            })
+    } catch (error) {
+        console.log(error)
+    }
 })
 app.get('/get-messages/:of', (REQ, RES) => {
-    Messages.findAll({
-        where: {
-            receiver: parseInt(REQ.params.of),
-            read: false,
-            received: false
-        },
-        raw: true
-    })
-        .then(u => {
-            Messages.update({
-                received: true
+    try {
+        Messages.findAll({
+            where: {
+                receiver: parseInt(REQ.params.of),
+                read: false,
+                received: false
             },
-                {
-                    where: {
-                        receiver: parseInt(REQ.params.of),
-                        read: false,
-                        received: false
-                    }
-                }
-            )
-                .then(r => {
-                    RES.send(u)
-                })
+            raw: true
         })
+            .then(u => {
+                Messages.update({
+                    received: true
+                },
+                    {
+                        where: {
+                            receiver: parseInt(REQ.params.of),
+                            read: false,
+                            received: false
+                        }
+                    }
+                )
+                    .then(r => {
+                        RES.send(u)
+                    })
+            })
+    } catch (error) {
+        console.log(error)
+    }
 })
 app.get('/get-requests/:user_id', (REQ, RES) => {
-    RequestModal.findAll({
-        where: {
-            to: parseInt(REQ.params.user_id),
-        },
-        raw: true
-    })
-        .then(u => {
-            RES.send(u)
+    try {
+        RequestModal.findAll({
+            where: {
+                to: parseInt(REQ.params.user_id),
+            },
+            raw: true
         })
+            .then(u => {
+                RES.send(u)
+            })
+    } catch (error) {
+        console.log(error)
+    }
 })
 app.post('/read-message', (REQ, RES) => {
-    var res_ids = REQ.body.message_ids;
-    res_ids = res_ids.replace(/[\[\]']+/g, '');
-    var ids = res_ids.split(",");
-    Messages.update({
-        read: true
-    }, {
-        where: {
-            id: ids
-        }
-    }).then(u => {
-        if (u) {
-            RES.sendStatus(200)
-        }
-    })
+    try {
+        var res_ids = REQ.body.message_ids;
+        res_ids = res_ids.replace(/[\[\]']+/g, '');
+        var ids = res_ids.split(",");
+        Messages.update({
+            read: true
+        }, {
+            where: {
+                id: ids
+            }
+        }).then(u => {
+            if (u) {
+                RES.sendStatus(200)
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 app.get('/accept-request/:id', (REQ, RES) => {
-    RequestModal.destroy({
-        where: {
-            id: REQ.params.id
-        }
-    })
-    .then(u => {
-        RES.sendStatus(200)
-    })
+    try {
+        RequestModal.destroy({
+            where: {
+                id: REQ.params.id
+            }
+        })
+            .then(u => {
+                RES.sendStatus(200)
+            })
+    } catch (error) {
+        console.log(error)
+    }
 })
 app.get('/reject-request/:id', (REQ, RES) => {
-    RequestModal.destroy({
-        where: {
-            id: REQ.params.id
-        }
-    })
-    .then(u => {
-        RES.sendStatus(200)
-    })
+    try {
+        RequestModal.destroy({
+            where: {
+                id: REQ.params.id
+            }
+        })
+            .then(u => {
+                RES.sendStatus(200)
+            })
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 app.listen('2643')
