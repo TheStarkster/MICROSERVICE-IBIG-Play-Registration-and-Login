1ab36dca8f900ccf53bfa54a8b654c3d5b1e69e8
Gurkaran Singh <sgurkaran2000@gmail.com>
2020-03-10 17:03:15 +0530

    ..

--- index.js
+++ index.js
@@ -602,11 +602,11 @@ app.post("/wallet/credit", (REQ, RES) => {
   var bal;
   User.findAll({ where: { id: req.id }, raw: true }).then(u => {
     bal = u[0].paytm_bal;
-    console.log(parseFloat(bal == null ? 0 : bal) + parseFloat(req.amount))
+    bal = parseInt(bal == null ? 0 : bal) + parseInt(req.amount)
   });
   User.update(
     {
-      paytm_bal: parseFloat(bal == null ? 0 : bal) + parseFloat(req.amount),
+      paytm_bal: bal,
       paytm_orders: sequelize.fn(
         "array_append",
         sequelize.col("paytm_orders"),
